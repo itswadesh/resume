332fe903295d1849be3ecfea6b6e6064f88d7396
Swadesh Behera <itswadesh@gmail.com>
2020-05-01 19:53:06 +0530

    github link fix

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,5 @@
 <template>
-  <section
-    class="resume-section p-3 p-lg-5 d-flex align-items-center"
-    id="about"
-  >
+  <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
     <div class="w-100">
       <h1 class="mb-0">
         Swadesh
@@ -12,49 +9,31 @@
         Bhubaneswar · Odisha ·
         <a href="mailto:name@email.com">swadesh@codenx.com</a>
       </div>
-      <p class="lead mb-5">I am experienced in Vue.js and Node.js in which I produce high quality code for my clients that are both well performing and well tested. I focus on solving problems, not only technical problems but creating an awesome experience for the users of my software both visual and accessible. Mixing powerful tools and knowledge about the users brings visual UI's to life and leads to webpages being accessible and useful.</p>
+      <p
+        class="lead mb-5"
+      >I am experienced in Vue.js and Node.js in which I produce high quality code for my clients that are both well performing and well tested. I focus on solving problems, not only technical problems but creating an awesome experience for the users of my software both visual and accessible. Mixing powerful tools and knowledge about the users brings visual UI's to life and leads to webpages being accessible and useful.</p>
       <p class="lead mb-5">
         My recent open source project
         <a
-          href="https://github.com/itswadesh/sapper-commerce"
+          href="https://github.com/itswadesh/sapper-ecommerce"
           target="_blank"
           rel="noopener noreferrer"
         >Sapper Commerce</a>.
       </p>
       <div class="social-icons">
-        <a
-          href="https://linkedin.com/in/itswadesh"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://linkedin.com/in/itswadesh" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fab', 'linkedin']" />
         </a>
-        <a
-          href="https://github.com/itswadesh"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://github.com/itswadesh" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fab', 'github']" />
         </a>
-        <a
-          href="https://twitter.com/itswadesh"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://twitter.com/itswadesh" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fab', 'twitter']" />
         </a>
-        <a
-          href="https://www.facebook.com/itswadesh"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://www.facebook.com/itswadesh" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fab', 'facebook']" />
         </a>
-        <a
-          href="https://angularcode.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://angularcode.com" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fas', 'rss']" />
         </a>
       </div>
